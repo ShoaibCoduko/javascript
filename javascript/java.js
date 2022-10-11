59c54c899039ab93745c6617c5b2b497081581f6
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-10-11 22:15:02 +0500

    edd

--- javascript/java.js
+++ javascript/java.js
@@ -7,12 +7,12 @@ for(var i=0; i<10; i++){
     text.style.backgroundColor="teal";
     text.style.color="white";
     text.style.padding="30px";
-    text.style.paddingTop="60px";
-    text.style.paddingBottom="60px";
+    text.style.paddingTop="30px";
+    text.style.paddingBottom="30px";
     text.style.borderRadius="50%";
     text.style.marginBottom="2em";
     text.style.display="inline";
-    text.style.margin="5ch";
+    text.style.margin="auto";
   
 }
 if(i == 11){
@@ -25,28 +25,61 @@ else{
     console.log("your Not A good number in")
 }
 let day;
-switch(new Date().getDay()){
+switch(new Date().getMonth()){
     case 0:
-        day = "Sunday";
+        day = "01";
         break;
       case 1:
-        day = "Monday";
+        day = "02";
         break;
       case 2:
-        day = "Tuesday";
+        day = "03";
         break;
       case 3:
-        day = "Wednesday";
+        day = "04";
         break;
       case 4:
-        day = "Thursday";
+        day = "05";
         break;
       case 5:
-        day = "Friday";
+        day = "06";
         break;
       case  6:
-        day = "Saturday";
+        day = "07";
+        break;
+      case  7:
+        day = "08";
+        break;
+      case  8:
+        day = "09";
+        break;
+      case  9:
+        day = "10";
+        break;
+      case  10:
+        day = "11";
+        break;
+      case  11:
+        day = "12";
 
 }
 const text = document.querySelector("b");
 text.innerHTML=day;
+var firstName = "shoaibahmad";
+console.log(firstName.length);
+let logstr ="hi! my name is shoaib ahmad and i am living in pakistan cuntry";
+console.log(logstr.length);
+const listitem = document.querySelector(".pxs-1");
+listitem.style.backgroundColor="teal";
+listitem.style.color="white";
+console.log(listitem);
+let changs=document.querySelector("li");
+firstName.innerText;
+console.groupCollapsed(changs);
+document.all[21].textContent="yes sir";
+console.log(document.getElementsByTagName('body'));
+ var litem = document.getElementsByTagName('li');
+ console.log(litem.length);
+ 
+
+
