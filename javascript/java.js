d9b01fe8853dad9befd081e40bbdcd8678de6779
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-10-14 15:44:30 +0500

    ghj

--- javascript/java.js
+++ javascript/java.js
@@ -1,103 +1,208 @@
-var listern = document.querySelector('.listiten');
-// console.log(listern.parentNode);
-// listern.parentNode.style.backgroundColor="lightblue";
-// listern.parentNode.parentNode.style.backgroundColor="pink";
-// listern.parentNode.parentNode.parentNode.style.backgroundColor="lightgray";
-console.log(listern.childNodes[1]);
-listern.childNodes[1].textContent="hhhhfghj-1";
-listern.childNodes[5].style.backgroundColor="yellow";
-listern.lastElementChild.style.backgroundColor="red";
-listern.lastElementChild.textContent = "check Changs text now"; 
+// // var listern = document.querySelector('.listiten');
+// // // console.log(listern.parentNode);
+// // // listern.parentNode.style.backgroundColor="lightblue";
+// // // listern.parentNode.parentNode.style.backgroundColor="pink";
+// // // listern.parentNode.parentNode.parentNode.style.backgroundColor="lightgray";
+// // // console.log(listern.childNodes[1]);
+// // // listern.childNodes[1].textContent="hhhhfghj-1";
+// // // listern.childNodes[5].style.backgroundColor="yellow";
+// // // listern.lastElementChild.style.backgroundColor="red";
+// // // listern.lastElementChild.textContent = "check Changs text now"; 
 
+// // // console.log(listern.previousElementSibling);
+// // // listern.previousElementSibling.style.backgroundColor="lightyellow";
+// // // listern.previousElementSibling.style.padding="30px 20px 10px 50px";
+// // // listern.previousElementSibling.childNodes[1].textContent = "eee";
+// // // listern.previousElementSibling.childNodes[2].textContent = "yhjnjn";
+// // var mainspan = document.querySelector('.spam')
+// // var newh1 =document.createElement("h1");
+// // newh1.className="header";
+// // newh1.id= "idheader";
+// // console.log(newh1);
+// // console.log(mainspan.parentNode); 
 
+var onclick = document.getElementById('clickbtn').addEventListener(
+  'click', check
+);
+function check(){
+ document.getElementById('output').innerHTML=e.target.value;
 
-
-
-
-
-
-console.log("hello World");
-for(var i=0; i<10; i++){
-    console.log(i);
-    const text = document.querySelector("b");
-    text.innerHTML="0";
-    text.innerHTML=i;
-    text.style.backgroundColor="teal";
-    text.style.color="white";
-    text.style.padding="30px";
-    text.style.paddingTop="30px";
-    text.style.paddingBottom="30px";
-    text.style.borderRadius="50%";
-    text.style.marginBottom="2em";
-    text.style.display="inline";
-    text.style.margin="auto";
   
+
 }
-if(i == 11){
-    for(var j=0; j<12; j++)
-    {
-        console.log(j)
-    }
-}
-else{
-    console.log("your Not A good number in")
-}
+//   var intext=document.getElementById('demo');
+//   intext.innerHTML="hello daer Student";
+//   intext.style.backgroundColor="green";
+//   intext.style.textAlign="center";
+//   intext.style.color="pink";
+
+
+
+
+
+//   // var listern = document.querySelector('.listiten'); 
+//   // listern.style.backgroundColor="pink";
+//   // var changestext =document.getElementById("px1");
+//   // changestext.textContent="all table delete now";
+//   // changestext.style.padding="20px 2em 3ch 3in";
+//   // changestext.style.display="inline";
+//   // changestext.style.margin="auto";
+//   // changestext.style.color="red";
+//   // console.log(e.clientX+" x value");
+//   // console.log(e.clientY+" y value");
+//   // console.log(e.altkey);
+//   // console.log(e.ctrlkey);
+//   // console.log(e.shiftkey);
+// }
+
+
+// console.log("hello World");
+// for(var i=0; i<10; i++){
+//     console.log(i);
+//     const text = document.querySelector("b");
+//     text.innerHTML="0";
+//     text.innerHTML=i;
+//     text.style.backgroundColor="teal";
+//     text.style.color="white";
+//     text.style.padding="30px";
+//     text.style.paddingTop="30px";
+//     text.style.paddingBottom="30px";
+//     text.style.borderRadius="50%";
+//     text.style.marginBottom="2em";
+//     text.style.display="inline";
+//     text.style.margin="auto";
+  
+// }
+// if(i == 11){
+//     for(var j=0; j<12; j++)
+//     {
+//         console.log(j);
+//     }
+// }
+// else{
+//     console.log("your Not A good number in")
+// }
+// let Month;
+// switch(new Date().getMonth()){
+//     case 0:
+//         Month = "01";
+//         break;
+//       case 1:
+//         Month = "02";
+//         break;
+//       case 2:
+//         Month = "03";
+//         break;
+//       case 3:
+//         Month = "04";
+//         break;
+//       case 4:
+//         Month = "05";
+//         break;
+//       case 5:
+//         Month = "06";
+//         break;
+//       case  6:
+//         Month = "07";
+//         break;
+//       case  7:
+//         Month = "08";
+//         break;
+//       case  8:
+//         Month = "09";
+//         break;
+//       case  9:
+//         Month = "10";
+//         break;
+//       case  10:
+//         Month = "11";
+//         break;
+//       case  11:
+//         Month = "12";
+
+// }
+// // const text = document.querySelector("b");
+// // text.innerHTML=day;
+// // var firstName = "shoaibahmad";
+// // console.log(firstName.length);
+// // let logstr ="hi! my name is shoaib ahmad and i am living in pakistan cuntry";
+// // console.log(logstr.length);
+// // const listitem = document.querySelector(".pxs-1");
+// // listitem.style.backgroundColor="teal";
+// // listitem.style.color="white";
+// // console.log(listitem);
+// // let changs=document.querySelector("li");
+// // firstName.innerText;
+// // console.groupCollapsed(changs);
+// // document.all[21].textContent="yes sir";
+// // console.log(document.getElementsByTagName('body'));
+// //  var litem = document.getElementsByTagName('li');
+// //  console.log(litem.length);
+
 let day;
 switch(new Date().getMonth()){
-    case 0:
-        day = "01";
-        break;
-      case 1:
-        day = "02";
-        break;
-      case 2:
-        day = "03";
-        break;
-      case 3:
-        day = "04";
-        break;
-      case 4:
-        day = "05";
-        break;
-      case 5:
-        day = "06";
-        break;
-      case  6:
-        day = "07";
-        break;
-      case  7:
-        day = "08";
-        break;
-      case  8:
-        day = "09";
-        break;
-      case  9:
-        day = "10";
-        break;
-      case  10:
-        day = "11";
-        break;
-      case  11:
-        day = "12";
-
+  case 0:
+  day = "1";
+  break;
+  case 1:
+  day = "2";
+  break;
+  case 2:
+  day = "3";
+  break;
+  case 3:
+  day = "4";
+  break;
+  case 4:
+  day = "5";
+  break;
+  case 5:
+  day = "6";
+  break;
+  case 6:
+  day = "7";
+  break;
+  case 7:
+    day = "8";
+    break;
+    case 8:
+    day = "9";
+    break;
+    case 9:
+    day = "10";
+    break;
+    case 10:
+    day = "11";
+    break;
+    case 11:
+    day = "12";
+    break;
+  
 }
-const text = document.querySelector("b");
-text.innerHTML=day;
-var firstName = "shoaibahmad";
-console.log(firstName.length);
-let logstr ="hi! my name is shoaib ahmad and i am living in pakistan cuntry";
-console.log(logstr.length);
-const listitem = document.querySelector(".pxs-1");
-listitem.style.backgroundColor="teal";
-listitem.style.color="white";
-console.log(listitem);
-let changs=document.querySelector("li");
-firstName.innerText;
-console.groupCollapsed(changs);
-document.all[21].textContent="yes sir";
-console.log(document.getElementsByTagName('body'));
- var litem = document.getElementsByTagName('li');
- console.log(litem.length);
- 
+console.log(day);
+// let timeout;
+// switch(new Date().getTime()){
+//   case 0:
+//   timeout = "00";
+//   break;
+//   case 1:
+//   timeout = "01";
+//   break;
+//   case 2:
+//   timeout = "02";
+//   break;
+//   case 3:
+//   timeout = "03";
+//   break;
 
+// }
+// console.log(timeout);
+var democlick = document.getElementById('democlick').addEventListener(
+  'click',democlick 
+);
+function democlick(){
+  var entertext = document.getElementById('demo');
+  entertext.textContent="xjocc";
 
+console.log(entertext)
+}
